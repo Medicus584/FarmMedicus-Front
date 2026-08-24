da1afa500e3261023234855bff148b27d67cd99a
AldoLima0918 <65356370+AldoLima0918@users.noreply.github.com>
2026-08-23 22:06:10 -0400

    fix

--- src/components/dashboard/FormularioProductos.tsx
+++ src/components/dashboard/FormularioProductos.tsx
@@ -857,7 +857,7 @@ const ProductoSimilarSelect = ({
             onChange={handleInputChange}
             onClick={handleInputClick}
             onFocus={() => setIsOpen(true)}
-            placeholder="Buscar por nombre o descripción..."
+            placeholder="Buscar por n. comercial o n. generico..."
             className="h-9 text-sm pl-9 w-full pr-8 cursor-pointer"
           />
           <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 h-4 w-4 text-muted-foreground" />
